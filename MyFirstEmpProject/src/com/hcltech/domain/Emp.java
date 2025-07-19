17f5d3426c0fcdf971687c3a901c5e55c800e3a8
Bachina Hema Sri <bachina.hemasri@hcltech.com>
2025-07-19 12:36:53 +0530

    Hema added class Emp

--- MyFirstEmpProject/src/com/hcltech/domain/Emp.java
+++ MyFirstEmpProject/src/com/hcltech/domain/Emp.java
@@ -1,5 +1,37 @@
 package com.hcltech.domain;
 
 public class Emp {
+	private int empId;
+	private String empName;
+	private double empSal;
+	public Emp(int empId, String empName, double empSal) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empSal = empSal;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(double empSal) {
+		this.empSal = empSal;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Emp [empId = "+empId +" empName = "+empName+" empSal = "+empSal+" ]";
+	}
 
 }
